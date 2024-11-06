54fdef73d2113dcd2b5ecd0e43b0eac731d2a6ec
hello-aaditya <aadityakumar.prof@gmail.com>
2024-11-06 05:24:17 +0000

    Conversion Operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -30,4 +30,42 @@ let stringNumber = String(someNumber)
 console.log(typeof(stringNumber));
 console.log(stringNumber);
 
+// ***********************OPERATIONS******************************
+console.log("*******************************************************************");
 
+let value = 3
+
+let negValue = -value
+console.log(negValue);
+
+console.log("Addition: " + (2+2))
+console.log("Subtraction: " + (2-2))
+console.log("Multiplication: " + (2*2))
+console.log("Power: " + (2**4))
+console.log("Division: " + (2/2))
+console.log("Remainder: " + (2%2))
+
+console.log("*******************************************************************");
+let str1 = "3"
+let str2 = "5"
+let str3 = str1 + str2
+console.log(str3); //=> concatenation
+console.log("*******************************************************************");
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 3)
+console.log(1 + "2" + 3)
+console.log(7 + 2 + "3")
+console.log("*******************************************************************");
+
+console.log(true + 2)
+console.log(2 + true)
+
+console.log(true + 2 + 7)
+console.log(2 + true + 7)
+console.log(2 + 7 + true)
+console.log("*******************************************************************");
+
+console.log("21" + true)
+console.log(true + "21")
