a31290e573323d51837a548266fa80da5118c634
hello-aaditya <aadityakumar.prof@gmail.com>
2024-11-07 14:07:16 +0000

    String Manipulation

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -17,4 +17,22 @@ console.log(newString);
 const anotherString = gameName.slice(1, 3)
 console.log(anotherString);
 
+const newStringOne = "     Ramaswami     "
+console.log(newStringOne)
+console.log(newStringOne.trim());
 
+const url = "https://ramaswami.com/rama%20swami"
+console.log(url.replace("%20", "-"));
+
+console.log(url.includes("rama"));
+
+
+const paragraph = "I think Ruth's dog is cuter than your dog!"
+console.log(paragraph.replaceAll("dog", "Monkey"));
+
+
+const myExpression = "Happy! "
+console.log(`I feel ${myExpression.repeat(3)}`)
+
+const str1 = '456'
+console.log(str1.padStart(2, '0'))
