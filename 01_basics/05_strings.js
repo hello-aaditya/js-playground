a0af5cb632e6dd8746c449ed39f9d584e6d56f2a
hello-aaditya <aadityakumar.prof@gmail.com>
2024-11-07 05:30:39 +0000

    Strings in Javascript

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,20 @@
+const name = "Ramaswami"
+const averageScore = 80
+
+console.log(`Hello my name is ${name} and my average score in T20 is ${averageScore}`);
+
+const gameName = new String("SpiderMan")
+
+console.log(gameName[0]);
+console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('e'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(1, 3)
+console.log(anotherString);
+
+
