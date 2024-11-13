4e5c51b29d5b455929dd995b6b0cacfba83ac7db
hello-aaditya <aadityakumar.prof@gmail.com>
2024-11-13 18:44:28 +0000

    wWhile and do while loop in Javascripthile and

---
+++ 05_iterations/two.js
@@ -0,0 +1,16 @@
+//while
+/* 
+let myNum = 10
+while(myNum)
+{
+    console.log(myNum);
+    myNum--;
+} */
+
+let score = 15
+do
+{
+    console.log(`score: ${score}`);
+    score++
+}
+while(score<=20)
